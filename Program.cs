a46ac802f78f53dc0ac9414c60a27062ab2a6b3e
Haddouche Sara <haddouchesara89@gmail.com>
2025-03-26 15:50:53 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpNews
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
